44bf58592f0c8a0b6726036d80cea5fafee63abf
Jake Walder <jakerzw@hotmail.co.uk>
2018-01-11 11:57:36 +0000

    Commenting revamp

--- PGG_FlyingTeapot/Main.cpp
+++ PGG_FlyingTeapot/Main.cpp
@@ -43,17 +43,10 @@ bool InitGL()
 
 int main(int argc, char *argv[])
 {
-	// This is our initialisation phase
-
-	// SDL_Init is the main initialisation function for SDL
-	// It takes a 'flag' parameter which we use to tell SDL what systems we're going to use
-	// Here, we want to use SDL's video system, so we give it the flag for this
-	// Incidentally, this also initialises the input event system
-	// This function also returns an error value if something goes wrong
-	// So we can put this straight in an 'if' statement to check and exit if need be
+	// Initialise SDL
 	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
 	{
-		// Something went very wrong in initialisation, all we can do is exit
+		// Something went very wrong in initialisation -> Exit the game
 		std::cout<<"Whoops! Something went very wrong, cannot initialise SDL :("<<std::endl;
 		return -1;
 	}
@@ -77,86 +70,60 @@ int main(int argc, char *argv[])
 
 
 
-	// Now we have got SDL initialised, we are ready to create a window!
-	// These are some variables to help show you what the parameters are for this function
-	// You can experiment with the numbers to see what they do
+	//Create the window: Set the size and any options for an SDL window, including the title
 	int winPosX = 100;
 	int winPosY = 100;
 	int winWidth = 800;
 	int winHeight = 800;
-	SDL_Window *window = SDL_CreateWindow("My Window!!!",  // The first parameter is the window title
+	SDL_Window *window = SDL_CreateWindow("Asteroids!",
 		winPosX, winPosY,
 		winWidth, winHeight,
 		SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
-	// The last parameter lets us specify a number of options
-	// Here, we tell SDL that we want the window to be shown and that it can be resized
-	// You can learn more about SDL_CreateWindow here: https://wiki.libsdl.org/SDL_CreateWindow?highlight=%28\bCategoryVideo\b%29|%28CategoryEnum%29|%28CategoryStruct%29
-	// The flags you can pass in for the last parameter are listed here: https://wiki.libsdl.org/SDL_WindowFlags
 
-	// The SDL_CreateWindow function returns an SDL_Window
-	// This is a structure which contains all the data about our window (size, position, etc)
-	// We will also need this when we want to draw things to the window
-	// This is therefore quite important we don't lose it!
-
-
-
-
-
-
-	// The SDL_Renderer is a structure that handles rendering
-	// It will store all of SDL's internal rendering related settings
-	// When we create it we tell it which SDL_Window we want it to render to
-	// That renderer can only be used for this window
-	// (yes, we can have multiple windows - feel free to have a play sometime)
+	// Initialise the renderer
 	SDL_Renderer * renderer = SDL_CreateRenderer( window, -1, 0 );
-
-
-	// Now that the SDL renderer is created for the window, we can create an OpenGL context for it!
-	// This will allow us to actually use OpenGL to draw to the window
+	
+	// Create a context for the renderer
 	SDL_GLContext glcontext = SDL_GL_CreateContext( window );
 
-	// Call our initialisation function to set up GLEW and print out some GL info to console
+	// Call the initialisation function to set up GLEW and print GL info to console
 	if( !InitGL() )
 	{
 		return -1;
 	}
-
-
 	
-	// We are going to work out how much time passes from frame to frame
-	// We will use this variable to store the time at our previous frame
-	// This function returns the number of milliseconds since SDL was initialised
+	// Store the number of milliseconds passed since SDL was initialised
 	unsigned int lastTime = SDL_GetTicks();
-
-	
+		
 	
 	// Enable the depth test to make sure triangles in front are always in front no matter the order they are drawn
-	// When you do this, don't forget to clear the depth buffer at the start of each frame - otherwise you just get an empty screen!
 	glEnable(GL_DEPTH_TEST);
 
-	// The scene contains all the objects etc
+	// Create a scene to contain all the objects
 	Scene myScene;
 
-
-	// Ok, hopefully finished with initialisation now
-	// Let's go and draw something!
-
-	// These are controlled by the states of key presses
-	// They will be used to control the camera
+	// Initialise the booleans used for key presses
 	bool cmdRotateLeft = false, cmdRotateRight = false, cmdRotateUp = false, cmdRotateDown = false;
 	bool cmdRollLeft = false, cmdRollRight = false, cmdMoveForward = false, cmdMoveBackward = false, cmdThrustUp = false, cmdThrustDown = false;
 	bool cmdShoot = false, cmdHasShot = false, cmdSlowDown = false;
 	
+	// Create the player
 	Player *mainPlayer = myScene.GetPlayer();
 
+	// Create the vectors to store all of the lasers and asteroids of which there will be multiple
 	std::vector<Laser*> allLasers;
 	std::vector<Asteroid*> allAsteroids;
 
+	// These variables will be used to manipulate how often a new asteroid is released into the game
+	// Release time is the number of milliseconds in between each asteroid being released
+	// Release time will be reduced as more asteroids are released to increase the difficulty of the game
 	unsigned int releaseTime = 1000;
 	unsigned int previousRelease = 0;
 
+	// Number released is a counter for the amount of asteroids released in the game
 	int numberReleased = 0;
 
+	//Initialise the random number generator for use in setting the asteroids spawn point
 	srand(time(NULL));
 
 	// We are now preparing for our main loop (also known as the 'game loop')
